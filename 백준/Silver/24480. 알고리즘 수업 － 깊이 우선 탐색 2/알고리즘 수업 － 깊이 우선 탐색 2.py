51a613b71ebeb6cb9faafc0657cf4599853cdf66
KSE0514 <156265560+KSE0514@users.noreply.github.com>
2025-11-08 18:00:41 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1820 ms, Memory: 357928 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,30 @@
+import sys
+import heapq
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+def dfs(cur):
+    global num
+
+    while G[cur]:
+        nxt = heapq.heappop(G[cur]) * (-1)
+        if visited[nxt] == 0:
+            visited[nxt] = num
+            num += 1
+            dfs(nxt)
+
+N, M, R = map(int, input().split())
+G = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+num = 2
+for _ in range(M):
+    u, v = map(int, input().split())
+    heapq.heappush(G[u], -v)
+    heapq.heappush(G[v], -u)
+
+
+visited[R] = 1
+dfs(R)
+
+for n in visited[1:]:
+    print(n)
