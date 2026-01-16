11bc9ac483d7d196aa3c5b6ff8c806303c5d44f7
KSE0514 <156265560+KSE0514@users.noreply.github.com>
2026-01-16 15:45:20 +0900

    [Bronze V] Title: 새싹, Time: 60 ms, Memory: 11280 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    String[] st = new String[] {
+      "         ,r\'\"7",
+      "r`-_   ,\'  ,/",
+      " \\. \". L_r'",
+      "   `~\\/",
+      "      |",
+      "      |"
+    };
+    for (String s:st) {
+      System.out.println(s);
+    }
+  }
+}
