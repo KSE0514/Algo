b3cd8d80df30c93ab8dc832ad1ab703792e46f2f
KSE0514 <156265560+KSE0514@users.noreply.github.com>
2026-03-09 12:30:24 +0900

    [Bronze V] Title: 개수 세기, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+v = int(input())
+cnt = 0
+for i in range(N):
+    if arr[i] == v:
+        cnt += 1
+
+print(cnt)
